fe4cf71f7d41d27b17b1b470355b238d46f09170
turbo <turbo>
2002-11-21 10:38:27 +0000

    Design change! It's irrelevant wether there's mail in the queue or not! If SMTP is up on papadoc, then qmail will send the mail's there. If it isn't, THEN we run (restart qmail) in smtp mode.

--- test-smtp.pl
+++ test-smtp.pl
@@ -1,12 +1,11 @@
 #!/usr/bin/perl
 
-# $Id: test-smtp.pl,v 1.1 2002-11-21 10:26:38 turbo Exp $
+# $Id: test-smtp.pl,v 1.2 2002-11-21 10:38:27 turbo Exp $
 
-# 1. Check for undelivered mails in /var/qmail/queue
-# 2. Test for SMTP connection
+# Test for SMTP connection on papadoc
 #
-# => If 'yes' on both, restart qmail     ('full delivery mode')
-# => If 'no' on the first, restart qmail ('smtpd mode')
+# => If 'yes', restart qmail ('full delivery mode')
+# => If 'no',  restart qmail ('smtpd mode')
 
 use Net::Telnet;
 
@@ -29,20 +28,11 @@ sub check_smtp {
     return $ok;
 }
 
-# Check for mails in queue
-sub check_queue {
-    chdir("/var/qmail/queue/mess") || die "Can't change to queue dir, $!\n";
-    $mails = `find -type f -name '[0-9]*' | wc -l | sed 's\@ \@\@g'`;
-    chomp($mails);
-
-    return $mails;
-}
-
 # Check for running qmail-rspawn
 sub check_qmail {
     my($ps);
 
-    $ps = `ps | grep qmail-rspawn | grep -v grep`;
+    $ps = `/bin/ps axwww | grep qmail-rspawn | grep -v grep`;
     if($ps ne "") {
 	return 1;
     } else {
@@ -51,31 +41,25 @@ sub check_qmail {
 }
 
 # -----------------------------------
-if(&check_queue()) {
-    # Mail's in queue
-    if(&check_smtp()) {
-	# SMTP works
-	print "Got mail's ($mails) in queue, and smtp works.\n";
+if(&check_smtp()) {
+    # SMTP works
+    print "SMTP on papadoc is up.\n";
+    
+    if(! &check_qmail()) {
+	# No qmail-rspawn running
 
-	if(&check_qmail()) {
-	    # No qmail-rspawn running
-	    print "Restarting qmail in DELIVERY mode.\n";
-	    exec($qmail_stop); exec($qmail_start);
-	}
-    } else {
-	# SMTP down
-	print "Got mail's ($mails) in queue, but SMTP on papadoc is down!\n";
-
-	if(! &check_qmail()) {
-	    print "Restarting qmail in SMTP mode.\n";
-	    exec($qmail_stop); exec($qmail_smtp);
-	}
+	print "Restarting qmail in DELIVERY mode.\n";
+	system($qmail_stop); system($qmail_start);
     }
 } else {
-    # No mail in queue
-    if(! &check_qmail()) {
-	print "Restarting qmail in SMTP mode ($mails mails).\n";
-	exec($qmail_stop); exec($qmail_smtp);
+    # SMTP down
+    print "SMTP on papadoc is down!\n";
+    
+    if(&check_qmail()) {
+	# qmail-rspawn running
+
+	print "Restarting qmail in SMTP mode.\n";
+	system($qmail_stop); system($qmail_smtp);
     }
 }
 
