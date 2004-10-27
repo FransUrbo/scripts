8c426e580cfcaf2a10a64caf4fd4b83cd11d7007
turbo <turbo>
2004-10-27 08:30:56 +0000

    Possibility to only show the volume names (without partition etc).

--- list_afs_vols.sh
+++ list_afs_vols.sh
@@ -20,17 +20,19 @@ fi
 # --------------
 # Get the CLI options...
 if [ ! -z "$1" ]; then
-    TEMP=`getopt -o hf --long help,noformat -- "$@"`
+    TEMP=`getopt -o hfv --long help,noformat,volumes -- "$@"`
     eval set -- "$TEMP"
     while true ; do
 	case "$1" in
 	    -h|--help)
 		echo "Usage:   `basename $0` [option]"
-		echo "Options: -h,--help		Show this help"
-		echo "         -f,--noformat	Don't use tabs, separate colums with :"
+		echo "Options: -h,--help        Show this help"
+		echo "         -f,--noformat    Don't use tabs, separate colums with :"
+		echo "         -v,--volumes     List only volume names"    
 		exit 0
 		;;
 	    -f|--noformat)	NOFORMAT=1 ; shift ;;
+	    -v|--volumes)	VOLUMES_ONLY=1 ; shift ;;
 	    --)			shift ; AFSSERVER="$1" ; break ;; 
 	    *)			echo "Internal error!" ; exit 1 ;;
 	esac
@@ -45,7 +47,7 @@ PARTITIONS="$*"
 for part in $PARTITIONS; do
     set -- `mount | grep $part`
     dev="$1"
-    if [ -z "$dev" ]; then
+    if [ -z "$dev" -a -z "$VOLUMES_ONLY" ]; then
 	dev="[remote]"
     fi
 
@@ -56,14 +58,16 @@ for part in $PARTITIONS; do
 	echo 1 > $TMPFILE
 
 	set -- `echo $line`
-	if [ -z "$NOFORMAT" ]; then
-	    printf "%-10s %-8s %-30s %10s %-2s %8d%s %s\n" $dev $part $1 $2 $3 $4 $5 $6
-	else
+	if [ ! -z "$VOLUMES_ONLY" ]; then
+	    printf "%s\n" $1
+	elif [ ! -z "$NOFORMAT" ]; then
 	    printf "%s;%s;%s;%s;%s;%d%s;%s\n" $dev $part $1 $2 $3 $4 $5 $6
+	else
+	    printf "%-10s %-8s %-30s %10s %-2s %8d%s %s\n" $dev $part $1 $2 $3 $4 $5 $6
 	fi
     done
 
     gotvols=`cat $TMPFILE`
-    [ -z "$NOFORMAT" -a "$gotvols" != 0 ] && echo
+    [ -z "$NOFORMAT" -a "$gotvols" != 0 -a -z "$VOLUMES_ONLY" ] && echo
 done
 rm $TMPFILE
