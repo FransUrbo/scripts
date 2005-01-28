a3cbbbe6219102f7a2727e2fbbc295c20feb5499
turbo <turbo>
2005-01-28 10:29:02 +0000

    For some reason my /tmp partition is mounted read-only every now and then. If 'tempfile' fails, try to fix the problem (by remounting read-write). Same thing should be possible to do with the dump file (for example if the device is full), but I'm not quite sure how to do that nicely.

--- kprop.sh
+++ kprop.sh
@@ -1,13 +1,46 @@
 #!/bin/sh
 
-# $Id: kprop.sh,v 1.2 2003-03-29 09:45:42 turbo Exp $
+# $Id: kprop.sh,v 1.3 2005-01-28 10:29:02 turbo Exp $
 
 kdclist="kerberos2.bayour.com"
 
-# Dump the database
-kdb5_util dump /var/lib/krb5kdc/slave_datatrans
+fix_error() {
+    fs=`echo $1` ; msg=`echo $2` ; error=1
 
-TMPFILE=`tempfile -p kprp.`
+    while [ $error -ge 1 -a $error -le 3 ]; do
+	if echo $msg | grep -qi 'read-only'; then
+	    # Error: FS readonly
+
+	    mount -o remount,rw $fs
+	    if [ "$?" == "0" ]; then
+		error=0
+	    else
+		error=`expr $error + 1`
+	    fi
+	elif echo $msg | grep -qi 'No space left on device'; then
+	    # Error: FS full
+	    #fs=`mount | egrep 'ext[23] \(rw\)' | grep -v /vice | sed -e 's@.*on @@' -e 's@\ .*@@'`
+
+	    # Not sure how I should handle this...
+	fi
+    done
+
+    if [ "$error" == "1" ]; then
+	exit 1
+    fi
+}
+
+# Dump the database
+OUTPUT=`kdb5_util dump /var/lib/krb5kdc/slave_datatrans 2>&1`
+if [ "$?" == "1" ]; then
+    fix_error /var/lib/krb5kdc "$OUTPUT"
+fi
+
+TMPFILE=`tempfile -p kprp. 2>&1`
+if [ "$?" == "1" ]; then
+    fix_error /tmp "$TMPFILE"
+    TMPFILE=`tempfile -p kprp. 2>&1`
+fi
 
 # Progagate the database to the slave KDC's
 for kdc in $kdclist; do
