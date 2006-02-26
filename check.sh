517a94316e6f4959339eeac589cabb3fd783e146
turbo <turbo>
2006-02-26 09:54:51 +0000

    * A complete rewrite to be more extendable.   - No hardcoded error values (instead multiply each message with two). * Add checks for AFS, HTTP, SPAMd and IMAPd

--- check.sh
+++ check.sh
@@ -1,46 +1,118 @@
 #!/bin/sh
 
-# $Id: check.sh,v 1.3 2006-01-15 21:40:07 turbo Exp $
+# $Id: check.sh,v 1.4 2006-02-26 09:54:51 turbo Exp $
+TOT_ERR=
 
-TOT_ERR=0
-HOSTS="aurora fritz 51pegasi"
+HOSTS="212.214.70.50 212.214.70.55 212.214.70.51"
+DOMAINS="dagdrivarn.se bayour.com data-akut.se"
+
+# --------------
 for host in $HOSTS; do
-    ERROR=0
+    ERROR=0 ; x=1
     printf "Checking %10s: " $host
 
-    host -t ns dagdrivarn.se $host > /dev/null 2>&1
-    [ "$?" != 0 ] && ERROR=1
+    # ---- PING
+    echo -n "."
+    ping -c 5 -n -q $host > /dev/null 2>&1
+    [ "$?" != 0 ] && ERROR=`expr $ERROR + $x`
+    x=`expr $x \* 2`
 
-    host aurora $host > /dev/null 2>&1
-    [ "$?" != 0 ] && ERROR=`expr $ERROR + 2`
+    # ---- NS pointers
+    for domain in $DOMAINS; do
+	echo -n "."
+	host -s 5 -t ns $domain $host > /dev/null 2>&1
+	[ "$?" != 0 ] && ERROR=`expr $ERROR + $x`
 
-    host -t mx data-akut.se $host > /dev/null 2>&1
-    [ "$?" != 0 ] && ERROR=`expr $ERROR + 4`
+	x=`expr $x \* 2`
+    done
 
-    ldapsearch -x -h $host -LLL -b 'o=Bayour.COM,c=SE' ou=People \* OpenLDAPaci > /dev/null 2>&1
-    [ "$?" != 0 ] && ERROR=`expr $ERROR + 8`
+    # ---- MX pointers
+    for domain in $DOMAINS; do
+	echo -n "."
+	host -s 5 -t mx $domain $host > /dev/null 2>&1
+	[ "$?" != 0 ] && ERROR=`expr $ERROR + $x`
 
-    /usr/sbin/test-smtp.pl $host
-    [ "$?" != 0 ] && ERROR=`expr $ERROR + 16`
+	x=`expr $x \* 2`
+    done
+
+    # ---- LDAP
+    echo -n "."
+    ldapsearch -l 5 -x -h $host -LLL -b 'o=Bayour.COM,c=SE' ou=People \* OpenLDAPaci > /dev/null 2>&1
+    [ "$?" != 0 ] && ERROR=`expr $ERROR + $x`
+    x=`expr $x \* 2`
+
+    # ---- SMTP
+    echo -n "."
+    /usr/local/sbin/test-tcp.pl $host 25
+    [ "$?" != 0 ] && ERROR=`expr $ERROR + $x`
+    x=`expr $x \* 2`
+
+    # ---- AFS
+    for server in ptserver vlserver; do
+	echo -n "."
+	bos status -localauth $host | egrep -q "$server.*disabled|$server.*shutdown"
+	[ "$?" == 0 ] && ERROR=`expr $ERROR + $x`
+
+	x=`expr $x \* 2`
+    done
+
+    # ---- HTTP
+    echo -n "."
+    lynx -connect_timeout=10 -head -dump http://$host 2>&1 | grep -q '^HTTP.*OK$'
+    [ "$?" != 0 ] && ERROR=`expr $ERROR + $x`
+    x=`expr $x \* 2`
 
     printf "=> %2d\n" $ERROR
 
-    TOT_ERR=`expr $TOT_ERR + $ERROR`
+    # ---- SPAMD
+    echo -n "."
+    /usr/local/sbin/test-tcp.pl $host 783
+    [ "$?" != 0 ] && ERROR=`expr $ERROR + $x`
+    x=`expr $x \* 2`
+
+    # ---- Store this error
+    if [ "$ERROR" != 0 ]; then
+	if [ ! -z "$TOT_ERR" ]; then
+	    TOT_ERR="$TOT_ERR $ERROR"
+	else
+	    TOT_ERR=$ERROR
+	fi
+    fi
 done
 
+# --------------
+printf "Checking %10s: " imap
+/usr/local/sbin/test-tcp.pl aurora 143
+if [ "$?" != 0 ]; then
+    echo "=> ERROR"
+else
+    echo "=> OK"
+fi
+
+# --------------
 printf "Checking %10s: " provider
 /usr/bin/ldapsearch -LLL -x -H ldapi://%2fvar%2frun%2fslapd%2fldapi.provider \
     -b 'o=Bayour.COM,c=SE' ou=People \* OpenLDAPaci > /dev/null 2>&1
-[ "$?" != 0 ] && ERROR=`expr $ERROR + 32`
-printf "=> %2d\n" $ERROR
+if [ "$?" != 0 ]; then
+    echo "=> ERROR"
+else
+    echo "=> OK"
+fi
 
-if [ "$TOT_ERR" != 0 ]; then
-    echo
-    echo "Explanations:"
-    echo "  1: NS dagdrivarn.se"
-    echo "  2: A/CNAME Aurora"
-    echo "  4: MX data-akut.se"
-    echo "  8: ldapsearch"
-    echo " 16: smtp"
-    echo " 32: ldap provider"
+# --------------
+if [ "$TOT_ERR" != "" ]; then
+    echo ; echo "Explanations:"
+    echo "     1: PING"
+    echo "     2: NS dagdrivarn.se"
+    echo "     4: NS bayour.com"
+    echo "     8: NS data-akut.se"
+    echo "    16: MX dagdrivarn.se"
+    echo "    32: MX bayour.com"
+    echo "    64: MX data-akut.se"
+    echo "   128: LDAP search"
+    echo "   256: SMTP"
+    echo "   512: AFS/PTServer"
+    echo "  1024: AFS/VLServer"
+    echo "  2048: HTTP"
+    echo "  4096: SPAMD"
 fi
