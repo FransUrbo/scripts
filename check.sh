5dde46d7b7cab18384be8ef24d48386746236710
turbo <turbo>
2006-04-28 11:29:24 +0000

    Don't use 'x' as a counter of more errors. It was complicated figguring out WHERE/WHAT went wrong from a cryptic number. Instead use a short descriptor if/where the error occured.

--- check.sh
+++ check.sh
@@ -1,75 +1,69 @@
 #!/bin/sh
 
-# $Id: check.sh,v 1.5 2006-02-26 09:57:42 turbo Exp $
+# $Id: check.sh,v 1.6 2006-04-28 11:29:24 turbo Exp $
 TOT_ERR=
 
 HOSTS="212.214.70.50 212.214.70.55 212.214.70.51"
+#HOSTS="212.214.70.50 212.214.70.51"
 DOMAINS="dagdrivarn.se bayour.com data-akut.se"
 
 # --------------
 for host in $HOSTS; do
-    ERROR=0 ; x=1
+    ERROR=0
     printf "Checking %10s: " $host
 
     # ---- PING
     echo -n "."
     ping -c 5 -n -q $host > /dev/null 2>&1
-    [ "$?" != 0 ] && ERROR=`expr $ERROR + $x`
-    x=`expr $x \* 2`
-
-    # ---- NS pointers
-    for domain in $DOMAINS; do
+    if [ "$?" = 0 ]; then
+	# ---- NS pointers
+	for domain in $DOMAINS; do
+	    echo -n "."
+	    host -s 5 -t ns $domain $host > /dev/null 2>&1
+	    [ "$?" != 0 ] && ERROR="ns/$domain/$host;"
+	done
+	
+	# ---- MX pointers
+	for domain in $DOMAINS; do
+	    echo -n "."
+	    host -s 5 -t mx $domain $host > /dev/null 2>&1
+	    [ "$?" != 0 ] && ERROR="mx/$domain/$host;"
+	done
+	
+	# ---- LDAP
 	echo -n "."
-	host -s 5 -t ns $domain $host > /dev/null 2>&1
-	[ "$?" != 0 ] && ERROR=`expr $ERROR + $x`
-
-	x=`expr $x \* 2`
-    done
-
-    # ---- MX pointers
-    for domain in $DOMAINS; do
+	ldapsearch -l 10 -x -h $host -LLL -b 'o=Bayour.COM,c=SE' ou=People \* OpenLDAPaci > /dev/null 2>&1
+	[ "$?" != 0 ] && ERROR="ldapsearch;"
+	
+	# ---- SMTP
 	echo -n "."
-	host -s 5 -t mx $domain $host > /dev/null 2>&1
-	[ "$?" != 0 ] && ERROR=`expr $ERROR + $x`
-
-	x=`expr $x \* 2`
-    done
-
-    # ---- LDAP
-    echo -n "."
-    ldapsearch -l 5 -x -h $host -LLL -b 'o=Bayour.COM,c=SE' ou=People \* OpenLDAPaci > /dev/null 2>&1
-    [ "$?" != 0 ] && ERROR=`expr $ERROR + $x`
-    x=`expr $x \* 2`
-
-    # ---- SMTP
-    echo -n "."
-    /usr/local/sbin/test-tcp.pl $host 25
-    [ "$?" != 0 ] && ERROR=`expr $ERROR + $x`
-    x=`expr $x \* 2`
-
-    # ---- AFS
-    for server in ptserver vlserver; do
+	/usr/local/sbin/test-tcp.pl $host 25
+	[ "$?" != 0 ] && ERROR="smtp;"
+	
+	# ---- AFS
+	for server in ptserver vlserver; do
+	    echo -n "."
+	    bos status -localauth $host | egrep -q "$server.*disabled|$server.*shutdown"
+	    [ "$?" == 0 ] && ERROR="afs/$server;"
+	done
+	
+	# ---- HTTP
 	echo -n "."
-	bos status -localauth $host | egrep -q "$server.*disabled|$server.*shutdown"
-	[ "$?" == 0 ] && ERROR=`expr $ERROR + $x`
+	lynx -connect_timeout=10 -head -dump http://$host 2>&1 | grep -q '^HTTP.*OK$'
+	[ "$?" != 0 ] && ERROR="http;"
+	
+	# ---- SPAMD
+	echo -n "."
+	/usr/local/sbin/test-tcp.pl $host 783
+	[ "$?" != 0 ] && ERROR="spamd;"
+    else
+	ERROR="ping;"
+    fi
 
-	x=`expr $x \* 2`
-    done
 
-    # ---- HTTP
-    echo -n "."
-    lynx -connect_timeout=10 -head -dump http://$host 2>&1 | grep -q '^HTTP.*OK$'
-    [ "$?" != 0 ] && ERROR=`expr $ERROR + $x`
-    x=`expr $x \* 2`
-
-    # ---- SPAMD
-    echo -n "."
-    /usr/local/sbin/test-tcp.pl $host 783
-    [ "$?" != 0 ] && ERROR=`expr $ERROR + $x`
-    x=`expr $x \* 2`
-
+    # ---------------------
     # ---- Store this error
-    printf "=> %2d\n" $ERROR
+    printf "=> %s\n" $ERROR
     if [ "$ERROR" != 0 ]; then
 	if [ ! -z "$TOT_ERR" ]; then
 	    TOT_ERR="$TOT_ERR $ERROR"
@@ -96,22 +90,4 @@ if [ "$?" != 0 ]; then
     echo "=> ERROR"
 else
     echo "=> OK"
-fi
-
-# --------------
-if [ "$TOT_ERR" != "" ]; then
-    echo ; echo "Explanations:"
-    echo "     1: PING"
-    echo "     2: NS dagdrivarn.se"
-    echo "     4: NS bayour.com"
-    echo "     8: NS data-akut.se"
-    echo "    16: MX dagdrivarn.se"
-    echo "    32: MX bayour.com"
-    echo "    64: MX data-akut.se"
-    echo "   128: LDAP search"
-    echo "   256: SMTP"
-    echo "   512: AFS/PTServer"
-    echo "  1024: AFS/VLServer"
-    echo "  2048: HTTP"
-    echo "  4096: SPAMD"
 fi
