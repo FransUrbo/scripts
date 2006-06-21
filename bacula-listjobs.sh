65e45573c2b36d46781019c87e9f95d8ab6b402a
turbo <turbo>
2006-06-21 10:28:39 +0000

    * Include the CVS Id tag. * Remove hardcoded username/password/host values. Instead read /etc/bacula/.conn_details   (from my BaculaSNMP-Stats script(s)). * Optionally get a all jobs from/in a specific volume name. * Retreive the output vertically instead of horizontally.   - Complicated the way the header got printed and also the actual data.     Quite extensive rewrite...

--- bacula-listjobs.sh
+++ bacula-listjobs.sh
@@ -1,42 +1,143 @@
 #!/bin/sh
 
-count=0
+# $Id: bacula-listjobs.sh,v 1.2 2006-06-21 10:28:39 turbo Exp $
 
-mysql -h mysql -u bacula --password=BaCuLa bacula -Br -e \
-"SELECT JobId,Name,StartTime,EndTime,Type,Level,JobFiles,JobBytes,JobStatus \
-FROM Job WHERE NOT JobBytes='0' ORDER BY StartTime" | \
+if [ -f "/etc/bacula/.conn_details" ]; then
+    . /etc/bacula/.conn_details
+else
+    echo "Config file /etc/bacula/.conn_details does not exists!"
+    exit 1
+fi
+
+if [ "$CATALOG" = "mysql" ]; then
+    COMMAND="mysql -h $HOST -u $USERNAME --password=$PASSWORD $DB -E -e"
+else
+    echo "PostgreSQL not yet availible. Please edit $0"
+    exit 1
+fi
+
+COLS="JobId,Name,StartTime,EndTime,Type,Level,JobFiles,JobBytes,JobStatus"
+ORDER="ORDER BY StartTime"
+
+if [ -n "$1" ]; then
+    # Get MediaId for this VolumeName
+    MEDIAID=`$COMMAND "select MediaId from Media where VolumeName='$1'" | grep -v MediaId`
+    if [ -z "$MEDIAID" ]; then
+	echo "No such media!"
+	exit 1
+    fi
+	
+    # Get all JobId's for this Volume
+    JOBID=`$COMMAND "SELECT DISTINCT JobId FROM JobMedia WHERE MediaId=$MEDIAID ORDER BY JobId" | grep -v JobId`
+    if [ -z "$JOBID" ]; then
+	echo "No jobs associated with this media!"
+	exit 1
+    fi
+	
+    # Get information for all these JobId's
+    SQL="SELECT $COLS FROM Job WHERE NOT JobBytes='0' AND ("
+    for jobid in $JOBID; do
+	SQL="$SQL JobId='$jobid' OR "
+    done
+    SQL="`echo $SQL | sed 's@ OR$@ )@'`" # Remove the last (empty) ' OR'.
+    SQL="$SQL $ORDER" # Add select order
+else
+    # Get information on ALl jobs
+    SQL="SELECT $COLS FROM Job WHERE NOT JobBytes='0' $ORDER"
+fi
+
+printed_full_header=0
+count=1
+    
+$COMMAND "$SQL" | grep -v '^\*\*\*\*' | \
 while read line; do
     set -- `echo $line`
-
-    if [ "$count" == "0" ]; then
+	
+    header=`echo $1 | sed 's@:$@@'`
+    shift
+    data=`echo $*`
+#    printf "Header: '$header'\nData: '$data'\n"
+	
+    if [ "$printed_full_header" == "0" ]; then
 	# Header
-	printf "%6s %-18s %-21s %-21s %5s %7s %12s %9s\n" $1 $2 $3 $4 $6 $7 $8 $9
+	case "$count" in
+	    1)  printf "%6s "   $header
+		jobid=$data;;
+		    
+	    2)  printf "%-25s " $header
+		name=$data;;
+		    
+	    3)  printf "%-20s " $header
+		starttime=$data;;
+		    
+	    4)  printf "%-20s " $header
+		endtime=$data;;
+		    
+	    6)  printf "%-10s " $header
+		case "$data" in
+		    B) level="Base";;
+		    D) level="Diff";;
+		    F) level="Full";;
+		    I) level="Incr";;
+		    *) level="$data";;
+		esac
+		;;
+		    
+	    7)  printf "%-10s "   $header
+		jobfiles=$data;;
+		    
+	    8)  printf "%14s "  $header
+		jobbytes=$data;;
+		    
+	    9)  printf "%9s\n"   $header
+		case "$data" in
+		    A) jobstatus="Canceled";;
+		    T) jobstatus="Ok";;
+		    E) jobstatus="Error";;
+		    f) jobstatus="Failed";;
+		    *) jobstatus="$data";;
+		esac
+		    
+		printed_full_header=1
+		count=0;;
+	esac
+    elif [ -n "$jobid" -a -n "$name" -a -n "$starttime" -a \
+	   -n "$endtime" -a -n "$level" -a -n "$jobfiles" -a \
+	   -n "$jobbytes" -a -n "$jobstatus" ]; then
+	printf "%6s %-25s %-20s %-20s %-7s %11s %16s %-5s\n" \
+		$jobid "$name" "$starttime" "$endtime" $level \
+		$jobfiles $jobbytes $jobstatus
+		    
+	jobid= ; name= ; starttime= ; endtime= ; level=
+	jobfiles= ; jobbytes= ; jobstatus=
     else
-	case "$8" in
-	    D) type="Diff";;
-	    I) type="Incr";;
-	    F) type="Full";;
-	    *) type="$8";;
+	case "$count" in
+	    1)  jobid=$data;;
+	    2)  name=$data;;
+	    3)  starttime=$data;;
+	    4)  endtime=$data;;
+	    6)  case "$data" in
+		    B) level="Base";;
+		    D) level="Diff";;
+		    F) level="Full";;
+		    I) level="Incr";;
+		    *) level="$data";;
+	        esac
+	        ;;
+		    
+	    7)  jobfiles=$data;;
+	    8)  jobbytes=$data;;
+	    9)  case "$data" in
+		    A) jobstatus="Canceled";;
+		    T) jobstatus="Ok";;
+		    E) jobstatus="Error";;
+		    f) jobstatus="Failed";;
+		    *) jobstatus="$data";;
+	        esac
+	        count=0;;
 	esac
-	printf "%6s %-18s %-10s %-10s %-10s %-10s %-4s %9s " $1 $2 $3 $4 $5 $6 $type $9
-
-	i=1
-	while [ $i -le 9 ]; do
-	    shift
-	    i=`expr $i + 1`
-	done
-
-	case "$2" in
-	    A) stat="Canceled";;
-	    T) stat="Ok";;
-	    E) stat="Error";;
-	    *) stat="$2";;
-	esac
-	printf "%12s %-9s\n" $1 $stat
     fi
-
+	
     count=`expr $count + 1`
 done
 
-# _ALL_ jobs:
-#"SELECT JobId,Name,StartTime,Type,Level,JobFiles,JobBytes,JobStatus FROM Job ORDER BY StartTime"
