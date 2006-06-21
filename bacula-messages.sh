77517440fb48010236bd10a2ad986c0905b60b27
turbo <turbo>
2006-06-21 10:30:26 +0000

    Include the CVS Id tag.

--- bacula-messages.sh
+++ bacula-messages.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+# $Id: bacula-messages.sh,v 1.2 2006-06-21 10:30:26 turbo Exp $
+
 echo 'messages' | bconsole | \
     egrep -v '^Connecting to Director|^1000 OK: |^Enter a period to cancel a command|^messages$'
--- bacula-status.sh
+++ bacula-status.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+# $Id: bacula-status.sh,v 1.2 2006-06-21 10:30:26 turbo Exp $
+
 echo "status dir" | bconsole | \
     egrep -v '^Connecting to Director|^1000 OK: |^Enter a period to cancel a command|^status dir$|^Using default Catalog name=|-dir Version: |^Daemon started |^==='
