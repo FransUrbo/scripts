be10eb239dbaf01e25c0fdc5b2a008595eae095c
turbo <turbo>
2006-06-21 10:20:53 +0000

    * Include the CVS Id tag. * Remove hardcoded username/password/host values. Instead read /etc/bacula/.conn_details   (from my BaculaSNMP-Stats script(s)).

--- bacula-listfiles.sh
+++ bacula-listfiles.sh
@@ -1,13 +1,27 @@
 #!/bin/sh
 
+# $Id: bacula-listfiles.sh,v 1.2 2006-06-21 10:20:53 turbo Exp $
+
 if [ -z "$1" ]; then
-    echo "Usage: `basename $0` [jobid]"
+    echo "Usage: `basename $0` <jobid>"
+    exit 1
+else
+    jobid=$1
+fi
+
+if [ -f "/etc/bacula/.conn_details" ]; then
+    . /etc/bacula/.conn_details
+else
+    echo "Config file /etc/bacula/.conn_details does not exists!"
     exit 1
 fi
 
-mysql -h mysql -u bacula --password=BaCuLa bacula -Br -e \
-"SELECT \
-CONCAT(Path.Path,Filename.Name) \
-AS Path \
-FROM File,Filename,Path \
-WHERE File.JobId=$1 AND Filename.FilenameId=File.FilenameId AND Path.PathId=File.PathId"
+if [ "$CATALOG" = "mysql" ]; then
+    COMMAND="mysql -h $HOST -u $USERNAME --password=$PASSWORD $DB -Br -e "
+else
+    echo "PostgreSQL not yet availible. Please edit $0"
+    exit 1
+fi
+
+$COMMAND "SELECT CONCAT(Path.Path,Filename.Name) AS Path FROM File,Filename,Path \
+WHERE File.JobId=$jobid AND Filename.FilenameId=File.FilenameId AND Path.PathId=File.PathId"
