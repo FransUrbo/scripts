94f1f31549f2a102441fdd6df571023c4d0bdf0e
Turbo Fredriksson <turbo@bayour.com>
2011-12-31 01:03:37 +0100

    Generate a 'Core frequency * Multiplier = CPU Frequency' combinations list. This to test every possible CPU speed possible in a structured way. Sorted by CPU Frequency. Semicolon separated for easy import to spreadsheet.

---
+++ generate_freq-mult_combos.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl -w                                                                                                                  
+
+$FREQ_MIN = 3300;
+$FREQ_MAX = 4800;
+
+$BASE_START = 100;
+$BASE_END = 600;
+$BASE_STEP = 1;
+
+$MULT_START = 4;
+$MULT_END = 31.5;
+$MULT_STEP = 0.5;
+
+for(my $mult = $MULT_START; $mult <= $MULT_END;) {
+    for(my $base = $BASE_START; $base <= $BASE_END;) {
+        $freq = sprintf("%0.4d", $base * $mult);
+	
+        if(($freq >= $FREQ_MIN) && ($freq <= $FREQ_MAX)) {
+#           print STDERR "$freq;$base;$mult\n";                                                                                     
+            $COMBOS{$freq} = "$base;$mult";
+	}
+
+        $base = $base + $BASE_STEP;
+    }
+    
+    $mult = $mult + $MULT_STEP;
+}
+
+for $key (sort keys %COMBOS) {
+    print "$key;".$COMBOS{$key}."\n";
+}
