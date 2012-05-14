9a6dd3a88d856f98d13da5980d7fecc74a7ef369
Turbo Fredriksson <turbo@bayour.com>
2012-05-14 23:29:19 +0200

    * Try harder to find the directory with the 'rev/model' files.   Once we have that, we know the correct device path. * Use 'lsscsi' command and then the '/sys/block/*/device' link   to find out the device name. * Only check /proc/mdstat if it exists (i.e., avoid 'no such file   or directory' error).   + Same with '/dev/disk/by-id'. * Modify output string size.

--- GetDiskInfo.sh
+++ GetDiskInfo.sh
@@ -9,7 +9,7 @@ if type zpool > /dev/null 2>&1; then
     zpool status > $ZFS_TEMP 2> /dev/null
 fi
 
-printf "  %-8s %-4s %-30s%-40s%-10s%-10s%-10s%-10s\n\n" "Host" "Name" "Model" "Device by ID" "Rev" "MD" "VG" "ZFS"
+printf "  %-7s %-4s %-20s%-45s%-10s%-10s%-10s%-10s\n\n" "Host" "Name" "Model" "Device by ID" "Rev" "MD" "VG" "ZFS"
 
 lspci -D | \
     egrep 'SATA|SCSI|IDE|RAID' | \
@@ -39,24 +39,50 @@ lspci -D | \
 
                 # ----------------------
                 # Get block path
-		blocks=`find $path/[0-9]*/block* 2> /dev/null`
+		blocks=`find $path/[0-9]*/block*/* -maxdepth 0 2> /dev/null`
                 if [ -z "$blocks" ]; then
 		    # Check again, below target* (for SCSI/SATA devices)
-                    blocks=`find $path/target*/[0-9]*/block* 2> /dev/null`
+                    blocks=`find $path/target*/[0-9]*/block*/* -maxdepth 0 2> /dev/null`
+                    if [ -z "$blocks" ]; then
+                        # Third attempt...
+                        blocks=`find $path/target*/[0-9]*/rev 2> /dev/null`
+                        if [ -n "$blocks" ]; then
+                            blocks=`find $path/target*/[0-9]* -maxdepth 0`
+                        fi
+                    fi
                 fi
 
                 # ----------------------
                 if [ -n "$blocks" ]; then
-                    find $blocks | \
+                    find $blocks/../.. -name 'rev' |
                         while read block; do
 			    # Reset path variable to actual/full path for this device
-			    path=`echo "$block" | sed 's@/block.*@@'`
+			    path=`echo "$block" | sed 's@/rev.*@@'`
+                            pushd $path > /dev/null 2>&1
+                              path=`/bin/pwd`
+                            popd > /dev/null 2>&1
+                            t_id=`basename "$path"`
 
                             # ----------------------
-                            # Get name, model and revision
-                            name=`find $path/block* | sed "s@.*:@@"`
+                            # Get name
+                            #name=`echo "$block" | sed "s@.*/block/\([a-z].*[a-z0-9]\)/\.\./.*/rev@\1@"`
+                            if type lsscsi  > /dev/null 2>&1; then
+                                name=`lsscsi --device "$t_id" | sed -e 's@.*/@@' -e 's@\[.*@@'`
+                            fi
+                            if [ -z "$name" -o "$name" == "-" ]; then
+                                # /sys/block/*/device | grep '/0000:05:00.0/host8/'
+                                name=`stat /sys/block/*/device | \
+					egrep "File: .*/$id/$host/" | \
+					sed "s@.*block/\(.*\)/device'.*@\1@"`
+                                if [ -z "$name" ]; then
+                                    name="n/a"
+                                fi
+                            fi
+
+                            # ... model
                             model=`cat "$path/model"`
-                            
+
+                            # ... and revision
                             if [ -f "$path/rev" ]; then
                                 rev="`cat \"$path/rev\"`"
                             else
@@ -64,55 +90,60 @@ lspci -D | \
                             fi
                             
                             # ----------------------
-                            # Get MD device
-                            MD=`grep $name /proc/mdstat | sed 's@: active raid1 @@'`
-                            if [ -n "$MD" ]; then
-                                # md3 sdg1[0] sdb1[1]
-                                set -- `echo "$MD"`
-                                for dev in $*; do
-                                    if echo "$dev" | grep -q "^$name[0-9]"; then
-                                        md="$1"
-                                        break
+                            DID="n/a" ; zfs="n/a"
+                            if [ -n "$name" -a "$name" != "n/a" ]; then
+                                if [ -f "/proc/mdstat" ]; then
+                                    # Get MD device
+                                    MD=`grep $name /proc/mdstat | sed 's@: active raid1 @@'`
+                                    if [ -n "$MD" ]; then
+                                        # md3 sdg1[0] sdb1[1]
+                                        set -- `echo "$MD"`
+                                        for dev in $*; do
+                                            if echo "$dev" | grep -q "^$name[0-9]"; then
+                                                md="$1"
+                                                break
+                                            fi
+                                        done
                                     fi
-                                done
-                            fi
+                                fi
                             
-                            # ----------------------
-			    # Get device name (Disk by ID)
-			    DID=`/bin/ls -l /dev/disk/by-id/$type* | grep -v part | grep $name | sed "s@.*/$type-\(.*\) -.*@\1@"`
-			    [ -z "$DID" ] && DID=n/a
+                                # ----------------------
+                                if [ -d "/dev/disk/by-id" ]; then
+                                    # Get device name (Disk by ID)
+                                    DID=`/bin/ls -l /dev/disk/by-id/$type* | grep -v part | grep $name | sed "s@.*/$type-\(.*\) -.*@\1@"`
+                                fi
 
-                            # ----------------------
-			    # Get ZFS pool data for this disk ID
-			    if [ -n "$DO_ZFS" ]; then
-				zfs=$(cat $ZFS_TEMP | 
-				    while read zpool; do
-					if echo "$zpool" | grep -q 'pool: '; then
-					    zfs_name=`echo "$zpool" | sed 's@.*: @@'`
-					elif echo "$zpool" | grep -q 'state: '; then
-					    zfs_state=`echo "$zpool" | sed 's@.*: @@'`
-					    shift ; shift ; shift ; shift ; shift
-					elif echo "$zpool" | egrep -q '^raid|^mirror|^cache|^spare'; then
-					    zfs_vdev=`echo "$zpool" | sed 's@ .*@@'`
-					elif echo "$zpool" | egrep -q "$DID|$name"; then
-					    if ! echo "$zpool" | egrep -q "ONLINE|AVAIL"; then
-						offline="!"
-						if echo "$zpool" | grep -q "OFFLINE"; then
-						    offline="$offline"OFFLINE
-						elif echo "$zpool" | grep -q "UNAVAIL"; then
-						    offline="$offline"UNAVAIL
-						elif echo "$zpool" | grep -q "FAULTED"; then
-						    offline="$offline"FAULTED
-						fi
-					    fi
-
-					    if [ "x$zfs_name" != "x" -a "x$zfs_vdev" != "x" ]; then
-						printf "%-17s$offline" "$zfs_name / $zfs_vdev"
-					    fi
-					fi
-				    done)
-			    fi
-			    [ -z "$zfs" ] && zfs=n/a
+                                # ----------------------
+                                # Get ZFS pool data for this disk ID
+                                if [ -n "$DO_ZFS" ]; then
+                                    zfs=$(cat $ZFS_TEMP | 
+                                        while read zpool; do
+                                            if echo "$zpool" | grep -q 'pool: '; then
+                                                zfs_name=`echo "$zpool" | sed 's@.*: @@'`
+                                            elif echo "$zpool" | grep -q 'state: '; then
+                                                zfs_state=`echo "$zpool" | sed 's@.*: @@'`
+                                                shift ; shift ; shift ; shift ; shift
+                                            elif echo "$zpool" | egrep -q '^raid|^mirror|^cache|^spare'; then
+                                                zfs_vdev=`echo "$zpool" | sed 's@ .*@@'`
+                                            elif echo "$zpool" | egrep -q "$DID|$name"; then
+                                                if ! echo "$zpool" | egrep -q "ONLINE|AVAIL"; then
+                                                    offline="!"
+                                                    if echo "$zpool" | grep -q "OFFLINE"; then
+                                                        offline="$offline"OFFLINE
+                                                    elif echo "$zpool" | grep -q "UNAVAIL"; then
+                                                        offline="$offline"UNAVAIL
+                                                    elif echo "$zpool" | grep -q "FAULTED"; then
+                                                        offline="$offline"FAULTED
+                                                    fi
+                                                fi
+                                                
+                                                if [ "x$zfs_name" != "x" -a "x$zfs_vdev" != "x" ]; then
+                                                    printf "%-17s$offline" "$zfs_name / $zfs_vdev"
+                                                fi
+                                            fi
+                                            done)
+                                fi
+                            fi
 
                             # ----------------------
                             # Get Virtual Group
@@ -133,10 +164,10 @@ lspci -D | \
                             
                             # ----------------------
                             # Output information
-			    printf "  %-8s %-4s %-30s%-40s%-10s%-10s%-10s%-10s\n" $host $name "$model" "$DID" $rev $md $vg "$zfs"
+			    printf "  %-7s %-4s %-20s%-45s%-10s%-10s%-10s%-10s\n" $host $name "$model" "$DID" $rev $md $vg "$zfs"
                     done
                 else
-                    printf "  %-8s n/a\n" $host
+                    printf "  %-7s n/a\n" $host
                 fi
         done
 
