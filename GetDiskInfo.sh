9d20d2832618c1c901ef84ba75131180afa79754
Turbo Fredriksson <turbo@bayour.com>
2012-05-15 10:44:56 +0200

    * Retabbing * Go directly to find 'rev' file if the '.../block' dir   doesn't exist.   + Try harder to find the 'rev' file (catch cciss targets etc) * Only call lsscsi if the device is a number (and not a name) * Make sure to return 'n/a' for ZFS if we really can't find it   (set 'zfs=n/a' in the correct place) * Rewrite the code that retreives Virtual Group (VG) using   'pvs' in a better way

--- GetDiskInfo.sh
+++ GetDiskInfo.sh
@@ -33,143 +33,143 @@ lspci -D | \
 	    sort | \
 		sed 's@.*;@@' | \
 		while read path; do
-		# ----------------------
-                # Get HOST ID
-		host=`echo "$path" | sed 's@.*/@@'`
+		    # ----------------------
+		    # Get HOST ID
+		    host=`echo "$path" | sed 's@.*/@@'`
 
-                # ----------------------
-                # Get block path
-		blocks=`find $path/[0-9]*/block*/* -maxdepth 0 2> /dev/null`
-                if [ -z "$blocks" ]; then
-		    # Check again, below target* (for SCSI/SATA devices)
-                    blocks=`find $path/target*/[0-9]*/block*/* -maxdepth 0 2> /dev/null`
-                    if [ -z "$blocks" ]; then
-                        # Third attempt...
-                        blocks=`find $path/target*/[0-9]*/rev 2> /dev/null`
-                        if [ -n "$blocks" ]; then
-                            blocks=`find $path/target*/[0-9]* -maxdepth 0`
-                        fi
-                    fi
-                fi
+		    # ----------------------
+		    # Get block path
+		    blocks=`find $path/[0-9]*/block*/* -maxdepth 0 2> /dev/null`
+		    if [ -z "$blocks" ]; then
+			# Check again - look for the 'rev' file. 
+			blocks=`find $path/target*/[0-9]*/rev 2> /dev/null`
+			if [ -n "$blocks" ]; then
+			    blocks=`find $path/target*/[0-9]* -maxdepth 0`
+			else
+			    # Third check - catch cciss targets.
+			    blocks=`find $path -name rev 2> /dev/null`
+			    if [ -n "$blocks" ]; then
+				blocks=`dirname $blocks`
+			    fi
+			fi
+		    fi
 
-                # ----------------------
-                if [ -n "$blocks" ]; then
-                    find $blocks/../.. -name 'rev' |
-                        while read block; do
-			    # Reset path variable to actual/full path for this device
-			    path=`echo "$block" | sed 's@/rev.*@@'`
-                            pushd $path > /dev/null 2>&1
-                              path=`/bin/pwd`
-                            popd > /dev/null 2>&1
-                            t_id=`basename "$path"`
+		    # ----------------------
+		    if [ -n "$blocks" ]; then
+			find $blocks/../.. -name 'rev' |
+			    while read block; do
+				# Reset path variable to actual/full path for this device
+				path=`echo "$block" | sed 's@/rev.*@@'`
+				pushd $path > /dev/null 2>&1
+				path=`/bin/pwd`
+				popd > /dev/null 2>&1
+				t_id=`basename "$path"`
 
-                            # ----------------------
-                            # Get name
-                            #name=`echo "$block" | sed "s@.*/block/\([a-z].*[a-z0-9]\)/\.\./.*/rev@\1@"`
-                            if type lsscsi  > /dev/null 2>&1; then
-                                name=`lsscsi --device "$t_id" | sed -e 's@.*/@@' -e 's@\[.*@@'`
-                            fi
-                            if [ -z "$name" -o "$name" == "-" ]; then
-                                # /sys/block/*/device | grep '/0000:05:00.0/host8/'
-                                name=`stat /sys/block/*/device | \
+				# ----------------------
+				# Get name
+				if echo "$t_id" | egrep -q "^[0-9]" -a type lsscsi > /dev/null 2>&1; then
+				    name=`lsscsi --device "$t_id" | sed -e 's@.*/@@' -e 's@\[.*@@'`
+				fi
+				if [ -z "$name" -o "$name" == "-" ]; then
+				    # /sys/block/*/device | grep '/0000:05:00.0/host8/'
+				    name=`stat /sys/block/*/device | \
 					egrep "File: .*/$id/$host/" | \
-					sed "s@.*block/\(.*\)/device'.*@\1@"`
-                                if [ -z "$name" ]; then
-                                    name="n/a"
-                                fi
-                            fi
+					sed -e "s@.*block/\(.*\)/device'.*@\1@" \
+				            -e 's@.*\!@@'`
+				    if [ -z "$name" ]; then
+					name="n/a"
+				    fi
+				fi
 
-                            # ... model
-                            model=`cat "$path/model"`
+				# ... model
+				model=`cat "$path/model"`
 
-                            # ... and revision
-                            if [ -f "$path/rev" ]; then
-                                rev="`cat \"$path/rev\"`"
-                            else
-                                rev="n/a"
-                            fi
-                            
-                            # ----------------------
-                            DID="n/a" ; zfs="n/a"
-                            if [ -n "$name" -a "$name" != "n/a" ]; then
-                                if [ -f "/proc/mdstat" ]; then
-                                    # Get MD device
-                                    MD=`grep $name /proc/mdstat | sed 's@: active raid1 @@'`
-                                    if [ -n "$MD" ]; then
-                                        # md3 sdg1[0] sdb1[1]
-                                        set -- `echo "$MD"`
-                                        for dev in $*; do
-                                            if echo "$dev" | grep -q "^$name[0-9]"; then
-                                                md="$1"
-                                                break
-                                            fi
-                                        done
-                                    fi
-                                fi
-                            
-                                # ----------------------
-                                if [ -d "/dev/disk/by-id" ]; then
-                                    # Get device name (Disk by ID)
-                                    DID=`/bin/ls -l /dev/disk/by-id/$type* | grep -v part | grep $name | sed "s@.*/$type-\(.*\) -.*@\1@"`
-                                fi
+				# ... and revision
+				if [ -f "$path/rev" ]; then
+				    rev="`cat \"$path/rev\"`"
+				else
+				    rev="n/a"
+				fi
 
-                                # ----------------------
-                                # Get ZFS pool data for this disk ID
-                                if [ -n "$DO_ZFS" ]; then
-                                    zfs=$(cat $ZFS_TEMP | 
-                                        while read zpool; do
-                                            if echo "$zpool" | grep -q 'pool: '; then
-                                                zfs_name=`echo "$zpool" | sed 's@.*: @@'`
-                                            elif echo "$zpool" | grep -q 'state: '; then
-                                                zfs_state=`echo "$zpool" | sed 's@.*: @@'`
-                                                shift ; shift ; shift ; shift ; shift
-                                            elif echo "$zpool" | egrep -q '^raid|^mirror|^cache|^spare'; then
-                                                zfs_vdev=`echo "$zpool" | sed 's@ .*@@'`
-                                            elif echo "$zpool" | egrep -q "$DID|$name"; then
-                                                if ! echo "$zpool" | egrep -q "ONLINE|AVAIL"; then
-                                                    offline="!"
-                                                    if echo "$zpool" | grep -q "OFFLINE"; then
-                                                        offline="$offline"OFFLINE
-                                                    elif echo "$zpool" | grep -q "UNAVAIL"; then
-                                                        offline="$offline"UNAVAIL
-                                                    elif echo "$zpool" | grep -q "FAULTED"; then
-                                                        offline="$offline"FAULTED
-                                                    fi
-                                                fi
-                                                
-                                                if [ "x$zfs_name" != "x" -a "x$zfs_vdev" != "x" ]; then
-                                                    printf "%-17s$offline" "$zfs_name / $zfs_vdev"
-                                                fi
-                                            fi
-                                            done)
-                                fi
-                            fi
+				# ----------------------
+				DID="n/a"
+				if [ -n "$name" -a "$name" != "n/a" ]; then
+				    if [ -f "/proc/mdstat" ]; then
+					# Get MD device
+					MD=`grep $name /proc/mdstat | sed 's@: active raid1 @@'`
+					if [ -n "$MD" ]; then
+					    # md3 sdg1[0] sdb1[1]
+					    set -- `echo "$MD"`
+					    for dev in $*; do
+						if echo "$dev" | grep -q "^$name[0-9]"; then
+						    md="$1"
+						    break
+						fi
+					    done
+					fi
+				    fi
 
-                            # ----------------------
-                            # Get Virtual Group
-                            if [ -n "$md" ]; then
-                                VG=`pvscan | grep $md`
-                                if [ -n "$VG" ]; then
-                                    # PV /dev/md4   VG movies   lvm2 [1,36 TB / 0    free]
-                                    vg=`echo "$VG" | sed -e 's@.*VG @@' -e 's@ .*@@'`
-                                else
-                                    vg="n/a"
-                                fi
-                                
-                                md="$md"`echo "$dev" | sed "s@.*[0-9]\[\([0-9]\)\]@/\1@"`
-                            else
-                                md="n/a"
-                                vg="n/a"
-                            fi
-                            
-                            # ----------------------
-                            # Output information
-			    printf "  %-7s %-4s %-20s%-45s%-10s%-10s%-10s%-10s\n" $host $name "$model" "$DID" $rev $md $vg "$zfs"
-                    done
-                else
-                    printf "  %-7s n/a\n" $host
-                fi
-        done
+				    # ----------------------
+				    if [ -d "/dev/disk/by-id" ]; then
+					# Get device name (Disk by ID)
+					DID=`/bin/ls -l /dev/disk/by-id/$type* | grep -v part | grep $name | sed "s@.*/$type-\(.*\) -.*@\1@"`
+				    fi
+
+				    # ----------------------
+				    # Get ZFS pool data for this disk ID
+				    if [ -n "$DO_ZFS" ]; then
+					zfs=$(cat $ZFS_TEMP | 
+					    while read zpool; do
+						if echo "$zpool" | grep -q 'pool: '; then
+						    zfs_name=`echo "$zpool" | sed 's@.*: @@'`
+						elif echo "$zpool" | grep -q 'state: '; then
+						    zfs_state=`echo "$zpool" | sed 's@.*: @@'`
+						    shift ; shift ; shift ; shift ; shift
+						elif echo "$zpool" | egrep -q '^raid|^mirror|^cache|^spare'; then
+						    zfs_vdev=`echo "$zpool" | sed 's@ .*@@'`
+						elif echo "$zpool" | egrep -q "$DID|$name"; then
+						    if ! echo "$zpool" | egrep -q "ONLINE|AVAIL"; then
+							offline="!"
+							if echo "$zpool" | grep -q "OFFLINE"; then
+							    offline="$offline"OFFLINE
+							elif echo "$zpool" | grep -q "UNAVAIL"; then
+							    offline="$offline"UNAVAIL
+							elif echo "$zpool" | grep -q "FAULTED"; then
+							    offline="$offline"FAULTED
+							fi
+						    fi
+
+						    if [ "x$zfs_name" != "x" -a "x$zfs_vdev" != "x" ]; then
+							printf "%-17s$offline" "$zfs_name / $zfs_vdev"
+						    fi
+						fi
+						done)
+					[ -z "$zfs" ] && zfs="n/a"
+				    fi
+
+				    # ----------------------
+				    # Get LVM data (VG - Virtual Group) for this disk
+				    md="n/a" ; vg="n/a"
+				    lvm_regexp="/$name"
+				    [ -n "$md" ] && lvm_regexp="$lvm_regexp|$md"
+				    if type pvs > /dev/null 2>&1; then
+					vg=$(pvs --noheadings --nosuffix --separator , | \
+					    while read pvs; do
+						if echo "$pvs" | egrep -q "$lvm_regexp"; then
+						    echo "$pvs" | sed "s@.*,\(.*\),lvm.*@\1@"
+						fi
+					    done)
+				    fi
+				fi
+
+				# ----------------------
+				# Output information
+				printf "  %-7s %-4s %-20s%-45s%-10s%-10s%-10s%-10s\n" $host $name "$model" "$DID" $rev $md $vg "$zfs"
+			    done # => 'while read block; do'
+		    else
+			printf "  %-7s n/a\n" $host
+		    fi
+		done # => 'while read path; do'
 
         echo
     done
