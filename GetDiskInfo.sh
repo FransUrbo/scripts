4e6846f706c256bbc29bb7ea34e5595d7ba682cf
Turbo Fredriksson <turbo@bayour.com>
2013-04-28 17:31:11 +0200

    * Fix check for lsscsi - should have been '&&', not '-a'. * lsscsi might sometimes add a space betwen the device and .. id?   /usr/local/Contego# lsscsi --device 0:0:0:0   [0:0:0:0]    disk    ATA      VBOX HARDDISK    1.0   /dev/sda[8:0]
      lsscsi --device 0:0:0:0
      [0:0:0:0] disk ATA SAMSUNG HD753LJ 1AA0 /dev/sda [8:0]

--- GetDiskInfo.sh
+++ GetDiskInfo.sh
@@ -116,8 +116,8 @@ lspci -D | \
 				# ----------------------
 				# Get name
 				name=""
-				if echo "$t_id" | egrep -q "^[0-9]" -a type lsscsi > /dev/null 2>&1; then
-				    name=`lsscsi --device "$t_id" | sed -e 's@.*/@@' -e 's@ \[.*@@'`
+				if echo "$t_id" | egrep -q "^[0-9]" && type lsscsi > /dev/null 2>&1; then
+				    name=`lsscsi --device "$t_id" | sed -e 's@.*/@@' -e 's@ \[.*@@' -e 's@\[.*@@'`
 				fi
 				if [ -z "$name" -o "$name" == "-" ]; then
 				    # /sys/block/*/device | grep '/0000:05:00.0/host8/'
