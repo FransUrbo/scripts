f140f14975560449fa1332e96ca758369a434b82
Turbo Fredriksson <turbo@bayour.com>
2014-08-28 22:23:32 +0000

    * Make ps and nc options definable at the top depending on if host is *BSD or Linux. * Make variables in the functions 'local' - "dset" in main() overwrote the same variable in the primary loop! * Remove a debug output (echo ret=$ret)

--- zfs-send+receive.sh
+++ zfs-send+receive.sh
@@ -7,15 +7,24 @@
 
 SSH_OPTS="-24Cx -o ConnectionAttempts=10 -o ConnectTimeout=60"
 GET_SNAP="zfs list -H -tsnapshot -oname -d1"
-PORT=$$ # Uncomment to run simple 'send | receive'
+#PORT=9000 # Uncomment to run simple 'send | receive'
+
+#PS_OPT="-Aw"				# *BSD
+PS_OPT="faxwww"				# Linux
+
+#NC_OPT_L="-4dvln $SSH_HOST $PORT"	# Listener (*BSD)
+NC_OPT_L="-l -p $PORT $SSH_HOST"	# Listener (Linux)
+
+#NC_OPT_R="-nw5"			# Receiver (Linux)
+NC_OPT_R="-n"				# Receiver (*BSD)
 
 # ------------------------------------------
 
 function get_local_snap {
-    fs="$1"
-    index=$2
+    local fs="$1"
+    local index=$2
 
-    i=0
+    local i=0
     $GET_SNAP "$dset" 2> /dev/null | \
 	sort -r | \
 	{ while read snaps; do
@@ -29,23 +38,24 @@ function get_local_snap {
 
 # Cleanup remote host
 function cleanup {
-    if [[ -n "$DSET" ]]; then
-	ssh $SSH_HOST ps -Aw \| grep -E \"nc \.\* $PORT$\|zfs receive \.\* $DSET\" \| grep -v \'grep \'
+    local pids
 
-	pids=`ssh $SSH_HOST ps -Aw \| grep -E \"nc \.\* $PORT$\|zfs receive \.\* $DSET\" \| grep -v \'grep \' | sed 's@ .*@@'`
+    if [[ -n "$DSET" ]]; then
+	pids=`ssh $SSH_HOST ps $PS_OPT \| grep -E \"nc \.\* $PORT$\|zfs receive \.\* $DSET\" \| grep -v \'grep \' | sed 's@ .*@@'`
 	[[ -n "$pids" ]] && ssh $SSH_HOST kill $pids
     fi
 }
 [[ -n "$PORT" ]] && trap cleanup EXIT
 
 function main {
-    dset="$dset"
-    snap_nr=$2
+    local dset="$1"
+    local snap_nr=0
 
     # Get latest local an remote snapshot
-    local_snap=$(get_local_snap "$dset" $snap_nr)
-    remote_snap=$(ssh -n $SSH_HOST $GET_SNAP \"$dset\" 2> /dev/null | tail -n1)
-    progres=$(/bin/ps faxwww | grep -w "${local_snap%@*}\"" | grep -v grep | wc -l)
+    local local_snap=$(get_local_snap "$dset" $snap_nr)
+    local remote_snap=$(ssh -n $SSH_HOST $GET_SNAP \"$dset\" 2> /dev/null | tail -n1)
+    local progres=$(/bin/ps faxwww | grep -w "${local_snap%@*}\"" | grep -v grep | wc -l)
+    progres=$(echo $progres) # Just remove leading zeros...
 
     if [[ -n "$DEBUG" ]]; then
 	echo "zfs send -p \"$local_snap\" | ssh $SSH_OPTS $SSH_HOST zfs receive -uvF \"$dset\""
@@ -65,34 +75,33 @@ function main {
 		main "$dset" $snap_nr
 	    fi
 	else
-	    ret=0 ; rm -f /tmp/rcv.$$*
+	    local ret=0 ; rm -f /tmp/rcv.$$*
 
 	    # Just to make sure that cleanup() don't kill something that shouldn't be killed!
-	    DSET="$dset"
+	    local DSET="$dset"
 
 	    # Startup a receiver on the remote host
-	    (ssh -n $SSH_HOST nc -4dvln $SSH_HOST $PORT \| zfs receive -uvF \"$dset\" 2>&1 | tee /tmp/rcv.$$-1) &
+	    (ssh -n $SSH_HOST nc $NC_OPT_L \| zfs receive -uvF \"$dset\" 2>&1 | tee /tmp/rcv.$$-1) &
 
 	    # Just make sure the receiver is up and running!
 	    sleep 2
 	    pid=$(/bin/ps faxwww | egrep "ssh .* nc .* zfs receive .*$DSET" | grep -v 'grep ' | sed 's@ [a-z].*@@')
 
 	    # Send the snapshot to netcat, which sends it to netcat on the receiver.
-	    (zfs send -p "$local_snap" | nc -nw5 $SSH_HOST $PORT 2>&1 | tee /tmp/rcv.$$-2)
+	    (zfs send -p "$local_snap" | nc $NC_OPT_R $SSH_HOST $PORT 2>&1 | tee /tmp/rcv.$$-2)
 
 	    # Cleanup local and remote (kill all process regarding this snapshot).
 	    # - netcat doesn't shutdown after the send/receive is completed...
 	    [[ -n "$pid" ]] && kill $pid
 
 	    # NOTE: Don't kill remote if send gave error - reuse this for the next snapshot in list...
-	    grep -Eq 'invalid backup stream|Invalid exchange' /tmp/rcv.$$* && ret=1
-	    echo "ret='$ret'"
+	    grep -Eq 'invalid backup stream|Invalid exchange|invalid name' /tmp/rcv.$$* && ret=1
 
 	    if [[ "$ret" -eq "1" ]]; then
 		((snap_nr += 1))
 		main "$dset" $snap_nr
 	    else
-		pids=`ssh $SSH_HOST ps -Aw \| grep -E \"nc \.\* $PORT$\|zfs receive \.\* $dset\" \| grep -v \'grep \' | sed 's@ .*@@'`
+		local pids=`ssh $SSH_HOST ps $PS_OPT \| grep -E \"nc \.\* $PORT$\|zfs receive \.\* $dset\"$" \| grep -v \'grep \' | sed 's@ .*@@'`
 		[ -n "$pids" ] && ssh $SSH_HOST kill $pids
 	    fi
 	fi
@@ -117,9 +126,7 @@ else
 fi
 
 # List all filesystems and volumes, get only the NAME column
-#zfs list -H -tfilesystem,volume -oname |
-cat /tmp/zfs_list.txt |
+zfs list -H -tfilesystem,volume -oname -r share/VirtualMachines |
     while read dset; do
-	nr=0
-	main "$dset" $nr
+	main "$dset"
     done
